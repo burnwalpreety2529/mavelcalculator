03bb12bc9b970e1963ca5c72aebdf0d9dd0e5597
burnwalpreety2529 <burnwalpreety2529@gmail.com>
2022-08-29 12:41:26 +0530

    first commit

---
+++ src/test/java/com/rishabh/calcm/CalcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.calcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
